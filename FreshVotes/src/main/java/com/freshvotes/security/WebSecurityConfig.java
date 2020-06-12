d5b51bc96b37a1687148a380cdb260cf07092986
Rick Ammon <rtammon@gmail.com>
2020-06-11 20:24:33 -0400

    getting started with spring security configuration

---
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -0,0 +1,34 @@
+package com.freshvotes.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    auth. inMemoryAuthentication()
+    .withUser("rtammon@gmail.com")
+    .password("appAccess")
+    .roles("USER");
+  }
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    http.csrf().disable()
+      .authorizeRequests()
+        .antMatchers("/").permitAll()
+        .anyRequest().hasRole("USER").and()
+      .formLogin()
+        .loginPage("/login")
+        .permitAll()
+        .and()
+      .logout()
+        .logoutUrl("/logout")
+        .permitAll();
+        
+  }
+
+}
