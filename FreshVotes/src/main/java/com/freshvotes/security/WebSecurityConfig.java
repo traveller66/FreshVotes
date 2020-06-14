cf62d0c529546800d9d8d4f2597df5e25a489dff
Rick Ammon <rtammon@gmail.com>
2020-06-14 09:03:19 -0400

    added password encoder and dashboard view

--- FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
+++ FreshVotes/src/main/java/com/freshvotes/security/WebSecurityConfig.java
@@ -1,26 +1,34 @@
 package com.freshvotes.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.web.util.matcher.AnyRequestMatcher;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+  
+  @Bean
+  public PasswordEncoder getPasswordEncoder() {
+    return new BCryptPasswordEncoder();
+  }
+  
   @Override
   protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-    auth. inMemoryAuthentication()
-    .withUser("rtammon@gmail.com")
-    .password("appAccess")
-    .roles("USER");
+    auth.inMemoryAuthentication()
+      .passwordEncoder(getPasswordEncoder())
+      .withUser("rtammon@gmail.com")
+      .password(getPasswordEncoder().encode("appAccess"))
+      .roles("USER");
   }
   @Override
   protected void configure(HttpSecurity http) throws Exception {
     http.csrf().disable()
       .authorizeRequests()
         .antMatchers("/").permitAll()
-        .antMatchers("/login").permitAll()
         .anyRequest().hasRole("USER").and()
       .formLogin()
         .loginPage("/login")
--- FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
+++ FreshVotes/src/main/java/com/freshvotes/web/DashboardController.java
@@ -1,14 +1,19 @@
 package com.freshvotes.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class DashboardController {
 
-	@RequestMapping(value="/", method=RequestMethod.GET)
+	//@RequestMapping(value="/", method=RequestMethod.GET)
+	@GetMapping("/")
 	public String rootView () {
 		return "index";
+	}
+	
+	@GetMapping("/dashboard")
+	public String dashboard() {
+	  return "dashboard";
 	}
 }
